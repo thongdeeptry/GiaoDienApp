01a495d49852852d918c44c4c6c3af130c276c87
Ngô Thành Thông <88930091+thongdeeptry@users.noreply.github.com>
2022-11-30 16:47:59 +0700

    Update Live Video

---
+++ src/layout/product/Call/screens/LiveScreen.js
@@ -0,0 +1,44 @@
+/** @format */
+
+import "expo-dev-client";
+import React, { useState, useEffect } from "react";
+import AgoraUIKit from "agora-rn-uikit";
+import { View } from "react-native";
+import { ongetTokenAgora } from "../utilities/getTokenAgora.context";
+const LiveVideo = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const [channel, setChannel] = useState("123");
+  const [role, setRole] = useState(1); //role = 2 people follow
+  const [uid, setUid] = useState(1);
+  const [expiry, setexpiry] = useState("9999999999999999999");
+  const [token, setToken] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const res = await ongetTokenAgora(channel, role, "uid", uid, expiry);
+      setToken(res);
+    }
+    fetchData();
+  }, []);
+  const props = {
+    connectionData: {
+      appId: "e63496cfe00f42d8be5c498370e6fa27",
+      channel: channel,
+      uid: uid,
+      token: token,
+    },
+    rtcCallbacks: {
+      EndCall: () => setVideoCall(false),
+    },
+  };
+
+  return videoCall ? (
+    <View style={{ width: "100%", height: "100%", paddingBottom: 50 }}>
+      <AgoraUIKit
+        connectionData={props.connectionData}
+        rtcCallbacks={props.rtcCallbacks}
+      />
+    </View>
+  ) : null;
+};
+
+export default LiveVideo;
